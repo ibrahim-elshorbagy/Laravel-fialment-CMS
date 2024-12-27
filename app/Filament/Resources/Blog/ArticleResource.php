d57b5f03d0d185e0e5ce76d9ee9fea62d2e9f216
ibrahim-elshorbagy <e.mohmed55@gmail.com>
2024-12-27 21:36:58 +0200

    Image Curator

---
+++ app/Filament/Resources/Blog/ArticleResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources\Blog;
+
+use App\Filament\Resources\Blog\ArticleResource\Pages;
+use App\Filament\Resources\Blog\ArticleResource\RelationManagers;
+use App\Models\Blog\Article;
+use Awcodes\Curator\Components\Forms\CuratorPicker;
+use Awcodes\Curator\Components\Tables\CuratorColumn;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ArticleResource extends Resource
+{
+    protected static ?string $model = Article::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title'),
+                TextInput::make('slug'),
+                TextInput::make('content'),
+                CuratorPicker::make('media_id'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                CuratorColumn::make('media_id')->size(100),
+                TextColumn::make('title')->searchable(),
+                TextColumn::make('slug'),
+
+                ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArticles::route('/'),
+            'create' => Pages\CreateArticle::route('/create'),
+            'edit' => Pages\EditArticle::route('/{record}/edit'),
+        ];
+    }
+}
